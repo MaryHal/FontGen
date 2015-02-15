229e5b7ebe22df4755b71891da157868a15e99a3
MaryHal <maryhal@users.noreply.github.com>
2015-02-14 17:08:36 -0800

    Initial version of OpenGLFont

---
+++ fontgen/OpenGLFont.hpp
@@ -0,0 +1,33 @@
+#ifndef _OpenGLFont_hpp_
+#define _OpenGLFont_hpp_
+
+#include <Font.hpp>
+
+namespace fgen
+{
+    class OpenGLFont : public Font
+    {
+        private:
+            unsigned int textureID;
+
+        public:
+            OpenGLFont(const std::string& filename,
+                       const std::vector<PackRange>& charRanges);
+            ~OpenGLFont();
+
+            // Copying
+            OpenGLFont(const OpenGLFont& that);
+            OpenGLFont& operator=(OpenGLFont that);
+
+            // Moving
+            OpenGLFont(OpenGLFont&& that);
+            OpenGLFont& operator=(OpenGLFont&& that);
+
+            void draw(float x, float y, const std::string& text);
+
+        private:
+            void makeOpenGLTexture();
+    };
+}
+
+#endif /* _OpenGLFont_hpp_ */
